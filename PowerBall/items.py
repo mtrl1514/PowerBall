0ef6b4ef625d50a00793192e749065c462f207ea
mtrl1514 <mtrl1514@gmail.com>
2018-05-25 20:56:24 +0900

    code crawl item

--- PowerBall/items.py
+++ PowerBall/items.py
@@ -6,9 +6,13 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.item import Item, Field
 
 
 class PowerballItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    lotteryDay = scrapy.Field()        #추첨일
+    lotteryCount = scrapy.Field()           #회차
+    lotteryNo = scrapy.Field()        #당첨번호
+    lotteryPowerBall = scrapy.Field()    #파워볼
+ 
+    
